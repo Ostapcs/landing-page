a87ab1c6a3c190275c2cc6b4f2f2888b04362e8f
Ostap Kutyanskyy <ostapkutya@gmail.com>
2019-10-10 18:17:30 +0300

    added animation for header and buy button,  added stagger parameter for svg file

--- Shop/js/main.js
+++ Shop/js/main.js
@@ -1,24 +1,52 @@
 $(document).ready(function () {
+    const buy = $('#buy');
+    const header = $("#header");
+    const scroll_to = $('.scroll-to');
 
-    $('.scroll-to').onScrolledTo(0, function () {
+    const w = $(window).width();
+    const h = $(window).height();
+    buy.css({left: w / 2 - buy.width() / 2 + "px"});
+    header.css({left: w / 2 - header.width() / 2 + "px", top: h / 2 - header.height() / 2 + 'px', display: "block"});
+
+    scroll_to.onScrolledTo(0, function () {
         const mySVG = $('#f').drawsvg({
             duration: 4000,
-            easing: 'linear'
+            easing: 'linear',
+            stagger: 350
         });
         mySVG.drawsvg('animate');
 
         const mySVG1 = $('#f1').drawsvg({
             duration: 4000,
-            easing: 'linear'
+            easing: 'linear',
+            stagger: 350
         });
         mySVG1.drawsvg('animate');
 
         const mySVG2 = $('#f2').drawsvg({
             duration: 4000,
-            easing: 'linear'
+            easing: 'linear',
+            stagger: 350
         });
         mySVG2.drawsvg('animate');
+    }, 1000);
+
+
+    $(window).scroll(function () {
+        let st = $(this).scrollTop();
+
+        buy.css({transform: 'translateX(0%) translateY(' + -st * 2 + '%)'});
+        header.css({transform: 'translateX(0%) translateY(' + -st * 1.5 + '%)'});
     });
+
+    $(window).resize(function () {
+        const w = $(window).width();
+        console.log(w + buy.width());
+
+        buy.css({left: w / 2 - buy.width() / 2 + "px"});
+
+    });
+
 
     AOS.init({
         easing: 'ease-in-out-sine'
