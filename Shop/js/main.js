1e842b1f362b5715b7bfc4d8bc4fd68b7be4fc4a
Ostap Kutyanskyy <ostapkutya@gmail.com>
2019-10-11 16:08:35 +0300

    change svg animation

--- Shop/js/main.js
+++ Shop/js/main.js
@@ -8,28 +8,12 @@ $(document).ready(function () {
     buy.css({left: w / 2 - buy.width() / 2 + "px"});
     header.css({left: w / 2 - header.width() / 2 + "px", top: h / 2 - header.height() / 2 + 'px', display: "block"});
 
+    
     scroll_to.onScrolledTo(0, function () {
-        const mySVG = $('#f').drawsvg({
-            duration: 4000,
-            easing: 'linear',
-            stagger: 350
-        });
-        mySVG.drawsvg('animate');
-
-        const mySVG1 = $('#f1').drawsvg({
-            duration: 4000,
-            easing: 'linear',
-            stagger: 350
-        });
-        mySVG1.drawsvg('animate');
-
-        const mySVG2 = $('#f2').drawsvg({
-            duration: 4000,
-            easing: 'linear',
-            stagger: 350
-        });
-        mySVG2.drawsvg('animate');
-    }, 1000);
+        new Vivus("f", {duration : 400, type: 'sync'}).play();
+        new Vivus("f1", {duration : 400, type: 'sync'}).play(2);
+        new Vivus("f2", {duration : 400, type: 'sync'}).play();
+    }, 0);
 
 
     $(window).scroll(function () {
@@ -47,11 +31,24 @@ $(document).ready(function () {
 
     });
 
+    $(".feed-back").slick({
+
+        dots: true
+    });
 
     AOS.init({
         easing: 'ease-in-out-sine'
     });
-
-
 });
+
+document.querySelector('.feed-back-text').onmousemove = (e) => {
+
+    const x = e.pageX - e.target.offsetLeft;
+    const y = e.pageY - e.target.offsetTop;
+
+    e.target.style.setProperty('--x', `${ x }px`);
+    e.target.style.setProperty('--y', `${ y }px`);
+
+};
+
 
