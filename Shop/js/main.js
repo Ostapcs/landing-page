9ba7678a7c02171acf203babb19b5953363fc39e
Ostap Kutyanskyy <ostapkutya@gmail.com>
2019-10-16 11:22:38 +0300

    code refactoring

--- Shop/js/main.js
+++ Shop/js/main.js
@@ -1,7 +1,6 @@
 $(document).ready(function () {
     const buy = $('#buy');
     const header = $("#header");
-    // const scroll_to = $('.scroll-to');
     const f = $('#f');
     const f1 = $('#f1');
     const f2 = $('#f2');
@@ -10,25 +9,24 @@ $(document).ready(function () {
     const h = $(window).height();
 
     buy.css({left: w / 2 - buy.width() / 2 + "px"});
-    header.css({left: w / 2 - header.width() / 2 + "px", top: h / 2 - header.height() / 2 + 'px', display: "block"});
+    header.css({left: w / 2 - header.width() / 2 + "px", top: h / 2 - header.height() / 2 + 'px', display: "block",});
 
     adaptiveFeature(w);
 
-    function adaptiveFeature(w){
-        if(w < 1050){
+    function adaptiveFeature(w) {
+        if (w < 1050) {
             $(".svg").each(function (i, obj) {
                 obj.classList.remove('col-3');
-                if(!obj.classList.contains("col-6"))
+                if (!obj.classList.contains("col-6"))
                     obj.classList.toggle("col-6");
             });
             $(".w-100").each(function (i, obj) {
                 obj.style.display = 'block'
             })
-        }
-        else{
+        } else {
             $(".svg").each(function (i, obj) {
                 obj.classList.remove('col-6');
-                if(!obj.classList.contains("col-3"))
+                if (!obj.classList.contains("col-3"))
                     obj.classList.toggle("col-3");
             });
             $(".w-100").each(function (i, obj) {
@@ -37,17 +35,17 @@ $(document).ready(function () {
         }
 
     }
-    
+
     f.onScrolledTo(0, function () {
-        new Vivus("f", {duration : 400, type: 'sync'}).play();
+        new Vivus("f", {duration: 400, type: 'sync'}).play();
     }, 0);
 
     f1.onScrolledTo(0, function () {
-        new Vivus("f1", {duration : 400, type: 'sync'}).play(2);
+        new Vivus("f1", {duration: 400, type: 'sync'}).play(2);
     }, 0);
 
     f2.onScrolledTo(0, function () {
-        new Vivus("f2", {duration : 400, type: 'sync'}).play();
+        new Vivus("f2", {duration: 400, type: 'sync'}).play();
     }, 0);
 
 
@@ -62,6 +60,8 @@ $(document).ready(function () {
         const w = $(window).width();
 
         buy.css({left: w / 2 - buy.width() / 2 + "px"});
+        header.css({left: w / 2 - header.width() / 2 + "px"});
+
 
         adaptiveFeature(w);
 
